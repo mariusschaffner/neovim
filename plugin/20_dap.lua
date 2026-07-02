97b5d6260414f88fe3b9199e3f4c1648545eb182
Marius Schaffner <marius.schaffner@vertec.com>
2026-07-02 07:16:14 +0200

    feat: rename files to force loading order

--- plugin/20_dap.lua
+++ plugin/20_dap.lua
@@ -1,12 +1,11 @@
 -- lua/plugins/dap.lua
 
 vim.pack.add({
-  { src = "https://github.com/mfussenegger/nvim-dap" },
-  { src = "https://github.com/rcarriga/nvim-dap-ui" },
-  { src = "https://github.com/nvim-neotest/nvim-nio" },
-  { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/jay-babu/mason-nvim-dap.nvim" },
-  { src = "https://github.com/mfussenegger/nvim-dap-python" },
+    { src = "https://github.com/mfussenegger/nvim-dap" },
+    { src = "https://github.com/rcarriga/nvim-dap-ui" },
+    { src = "https://github.com/nvim-neotest/nvim-nio" },
+    { src = "https://github.com/jay-babu/mason-nvim-dap.nvim" },
+    { src = "https://github.com/mfussenegger/nvim-dap-python" },
 })
 
 local dap   = require('dap')
@@ -23,31 +22,31 @@ vim.keymap.set('n', '<leader>du', function() dapui.toggle() end, { desc = '[D]eb
 -- previously the `config` function body, unchanged
 require('mason').setup({}) -- mason itself needs setup() now too, since vim.pack has no opts={} shorthand
 require('mason-nvim-dap').setup {
-  automatic_installation = true,
-  handlers = {},
-  ensure_installed = { "python" },
+    automatic_installation = true,
+    handlers = {},
+    ensure_installed = { "python" },
 }
 
 dapui.setup({
-  layouts = {
-    {
-      position = "left",
-      size = 40,
-      elements = {
-        { id = "scopes",      size = 0.5 },
-        { id = "breakpoints", size = 0.25 },
-        { id = "stacks",      size = 0.25 },
-      },
+    layouts = {
+        {
+            position = "left",
+            size = 40,
+            elements = {
+                { id = "scopes",      size = 0.5 },
+                { id = "breakpoints", size = 0.25 },
+                { id = "stacks",      size = 0.25 },
+            },
+        },
+        {
+            position = "bottom",
+            size = 12,
+            elements = {
+                { id = "repl",    size = 0.4 },
+                { id = "console", size = 0.6 },
+            },
+        },
     },
-    {
-      position = "bottom",
-      size = 12,
-      elements = {
-        { id = "repl",    size = 0.4 },
-        { id = "console", size = 0.6 },
-      },
-    },
-  },
 })
 
 vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
@@ -58,35 +57,35 @@ local breakpoint_icons = vim.g.have_nerd_font
     or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
 
 for type, icon in pairs(breakpoint_icons) do
-  local tp = 'Dap' .. type
-  local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
-  vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+    local tp = 'Dap' .. type
+    local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+    vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
 end
 
 local debug_hl_ns = vim.api.nvim_create_namespace("dap_debug_bg")
 
 local function set_debug_bg()
-  vim.api.nvim_set_hl(0, "NormalDebug", { bg = "#57606f" })
-  local session = require("dap").session()
-  if not session then return end
-  local program = session.config.program
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local buf = vim.api.nvim_win_get_buf(win)
-    local name = vim.api.nvim_buf_get_name(buf)
-    if name == program then
-      vim.api.nvim_win_call(win, function()
-        vim.opt_local.winhighlight = "Normal:NormalDebug"
-      end)
+    vim.api.nvim_set_hl(0, "NormalDebug", { bg = "#57606f" })
+    local session = require("dap").session()
+    if not session then return end
+    local program = session.config.program
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local name = vim.api.nvim_buf_get_name(buf)
+        if name == program then
+            vim.api.nvim_win_call(win, function()
+                vim.opt_local.winhighlight = "Normal:NormalDebug"
+            end)
+        end
     end
-  end
 end
 
 local function clear_debug_bg()
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    vim.api.nvim_win_call(win, function()
-      vim.opt_local.winhighlight = ""
-    end)
-  end
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        vim.api.nvim_win_call(win, function()
+            vim.opt_local.winhighlight = ""
+        end)
+    end
 end
 
 dap.listeners.after.event_initialized["debug_bg"]     = set_debug_bg
